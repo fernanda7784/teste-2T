bab42228279c21a26102206c0bf02600ee32e97c
fernanda7784 <160760245+fernanda7784@users.noreply.github.com>
2024-06-12 12:28:32 -0300

    12/06

---
+++ script.js
@@ -0,0 +1,19 @@
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const perguntas = [
+    "Pergunta 1",
+    "Pergunta 2"
+];
+const perguntas = [
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+
+    "Pergunta 2"
